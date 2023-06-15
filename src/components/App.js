3245ecaf79c3e45bcb2498d48c56812f2645864f
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-15 10:35:38 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [tab, setTab] = useState('Tab 1')
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul onClick={(e) => setTab(e.target.innerHTML)}>
+          <li>Tab 1</li>
+          <li>Tab 2</li>
+          <li>Tab 3</li>
+        </ul>
+        <p>This is the content for {tab}</p>
     </div>
   )
 }
